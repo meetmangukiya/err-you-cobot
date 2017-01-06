41a0ba91f176a5491358d4367c67edd421057189
Meet Mangukiya <meet123mangukiya@gmail.com>
2017-01-06 19:39:42 +0530

    config.py: Change the config vars with relative to cwd

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 import logging
+import os
 
 # This is a minimal configuration to get you started with the Text mode.
 # If you want to connect Errbot to chat services, checkout
 # the options in the more complete config-template.py from here:
 # https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
 
-BACKEND = 'Text'  # Errbot will start in text mode (console only mode) and will answer commands from there.
+BACKEND = 'Gitter'  # Errbot will start in text mode (console only mode) and will answer commands from there.
 
-BOT_DATA_DIR = r'/home/phoenista/Desktop/ghubby/cobot/err-you-cobot/data'
-BOT_EXTRA_PLUGIN_DIR = '/home/phoenista/Desktop/ghubby/cobot/err-you-cobot/plugins'
+BOT_DATA_DIR = os.path.join(os.getcwd(), 'data')
+BOT_EXTRA_BACKEND_DIR = os.path.join(os.getcwd(), 'err-backend-gitter')
 
-BOT_LOG_FILE = r'/home/phoenista/Desktop/ghubby/cobot/err-you-cobot/errbot.log'
+BOT_EXTRA_PLUGIN_DIR = os.path.join(os.getcwd(), 'plugins')
+
+BOT_LOG_FILE = os.path.join(os.getcwd(), 'errbot.log')
 BOT_LOG_LEVEL = logging.DEBUG
 
-BOT_ADMINS = ('CHANGE ME', )  # !! Don't leave that to "CHANGE ME" if you connect your errbot to a chat system !!
+BOT_ADMINS = ('meetmangukiya', )  # !! Don't leave that to "CHANGE ME" if you connect your errbot to a chat system !!
